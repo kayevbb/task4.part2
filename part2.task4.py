2cf29811ce2a1cad73bbbeb01157464cc04ca5e3
Bektur <tip.beka99@gmail.com>
2019-10-24 10:51:33 +0600

    ka

---
+++ part2.task4.py
@@ -0,0 +1,11 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+a1 = int(input())
+b1 = int(input())
+c1 = int(input())
+
+result = (a1 - a ) * 3600 + (b1 - b) * 60 + c1 - c
+
+print('Результат:', result)
